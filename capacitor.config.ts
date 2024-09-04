01af410f09a5b52f7e58277db4412a0be3942381
david cuspoca <david-cuspoca@hotmail.com>
2024-09-03 19:24:34 -0600

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'control-horas',
+  webDir: 'www'
+};
+
+export default config;
